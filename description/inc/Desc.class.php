fd0f0eb18d85b112359ad66c2275d064c9dfadb4
Santiago Ramirez <santiagorave98@gmail.com>
2023-05-23 19:54:35 -0700

    Mobile version of the description page in progress

--- description/inc/Desc.class.php
+++ description/inc/Desc.class.php
@@ -1,5 +1,71 @@
 <?php
 
     class Desc {
+
+        public static function body() : string{
+            $body = '<section class="body-desc">
+            <section class="gallery">
+                <img class="banner" src="./inc/img/img-1.jpg" alt="">
+                <figure>
+                    <img src="./inc/img/img-2.jpg" alt="img-1">
+                    <img src="./inc/img/img-3.jpg" alt="img-2">
+                    <img src="./inc/img/img-4.jpg" alt="img-3">
+                    <img src="./inc/img/img-5.jpg" alt="img-4">
+                </figure>
+            </section>
+            <section class="main-content">
+                <article class="left">
+                    <aside class="info-banner">
+                        <aside class="icons">
+                            <span><i class="fa-solid fa-map"></i>Acreage: 20ft</span>
+                            <span><i class="fa-regular fa-user"></i>Guests: 5</span>
+                            <span><i class="fa-solid fa-bed"></i>Bed: 2</span>
+                        </aside>
+                        <aside>
+                            <span>From: <span class="number">$120</span>/DAYS</span>           
+                        </aside>
+                    </aside>
+                    <article class="description">
+                        <strong>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error quo fugiat eveniet, velit dicta cupiditate alias! Sunt impedit quasi suscipit.</strong>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum accusantium voluptas sunt accusamus nihil natus similique debitis aut, modi autem qui ratione? Neque ipsa ducimus delectus consectetur voluptatibus odio libero explicabo deserunt fugiat obcaecati rerum praesentium maiores facilis quasi, nesciunt id tempora nisi, vitae itaque non perferendis. Autem, voluptatem dolorum.</p>
+                        <aside class="amenities">
+                       <strong> Amenities </strong>
+                            <ul>
+                                <li>Refrigerator and water</li>
+                                <li>Hairdryer and iron</li>
+                                <li>Washer</li>
+                                <li>Dryer</li>
+                            </ul>
+                        </aside>
+                    </article>
+                </article>
+                <article class="right">
+                    <aside class="block">
+                        <p>CHECK - IN</p>
+                        <span>18</span>
+                        <p>Jan, 2019 - Saturday</p>
+                        <button>CHANGE</button>
+                    </aside>
+                    <aside class="block">
+                        <p>CHECK - OUT</p>
+                        <span>21</span>
+                        <p>Feb, 2019 - Tuesday</p>
+                        <button>CHANGE</button>
+                    </aside>
+                    <aside class="block">
+                        <p>GUESTS</p>
+                        <span>3</span>
+                    </aside>
+                    <aside class="block">
+                        <p>Nights</p>
+                        <span>4</span>
+                    </aside>
+                    <button>BOOK NOW</button>
+                </article>
+            </section>
+        </section>';
+        return $body;
+
+        }
    }
     
--- description/index.php
+++ description/index.php
@@ -1,4 +1,5 @@
-<?php
+<?php   
+require_once("./inc/Desc.class.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -6,10 +7,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+    <link rel="stylesheet" href="../css/style.css">
     <title>Document</title>
 </head>
 <body>
-    <?php include  "./inc/gallery.html"?>
-</body>
+        <?php  echo Desc::body();  ?>
+    
+</body> 
 </html>
